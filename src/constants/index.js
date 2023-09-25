a6384c2e83d9654fd1a444f8f08cdb2affd94264
Dennis malware <davspencer30@gmail.com>
2023-09-25 11:19:55 +0300

    updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,3 +8,25 @@ export const NavLinks = [
     {href:"About", label: "#About"}
 ]
 
+export const Case = [
+    {
+        imgURL:"",
+        Step:"Step 1",
+        label:"Tell us how much you need.",
+        subtext:"Submit a quick form with your case details and cash required. We fund cases from $1,000 to $250,000."
+    },
+
+    {
+        imgURL:"",
+        Step:"Step 2",
+        label:"We review and approve.",
+        subtext:"We either auto-approve requests or gather details from your lawyer. We typically approve in 24 hrs."
+    } ,
+
+    {
+        imgURL:"",
+        Step:"Step 3",
+        label:"Get cash and fight your case.",
+        subtext:"We will advance you the money today with no obligation to make any payments unless you win or settle your lawsuit"
+    }
+]
