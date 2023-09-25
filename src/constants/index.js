31ff4095c00c336c2869d382829d99c43cac4635
Dennis malware <davspencer30@gmail.com>
2023-09-25 11:09:44 +0300

    added navlinks

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const NavLinks = [
+    {href:"Attorney", label: "#Attorney"},
+    {href:"Partners", label: "#Partners"},
+    {href:"Medical", label: "#Medical"}, 
+    {href:"Access", label:"#Access"}, 
+    {href:"Pass", label: "#Pass"},
+    {href:"FAQs", label: "#FAQs"},
+    {href:"About", label: "#About"}
+]
+
