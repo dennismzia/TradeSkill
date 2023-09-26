8b8084c7667d42a9496d2548949f49a9b6ebe56e
Dennis malware <davspencer30@gmail.com>
2023-09-26 10:04:40 +0300

    updates

---
+++ src/assets/images/index.js
@@ -0,0 +1,29 @@
+
+
+import ConstructionAccident from "./ConstructionAccident.svg"
+import DogBIte from "./DogBIte.svg"
+import Laborlaws from "./Laborlaws.svg"
+import medical from "./medical.svg"
+import motorvehicleaccident from "./motorvehicleaccident.svg "
+import premisesLiablity from "./premisesLiablity.svg "
+import productLiability from "./productLiability.svg" 
+import Railway from "./Railway.svg "
+import slipandfall from "./slipandfall.svg"
+import workAccident from "./workAccident.svg "
+import wrongfulDeath from "./wrongfulDeath.svg "
+import wrongfulImprisonment from "./wrongfulImprisonment.svg"
+
+export {
+    ConstructionAccident,
+    DogBIte,
+    Laborlaws,
+    medical,
+    motorvehicleaccident,
+    premisesLiablity,
+    productLiability,
+    Railway,
+    slipandfall,
+    workAccident,
+    wrongfulDeath,
+    wrongfulImprisonment
+}
