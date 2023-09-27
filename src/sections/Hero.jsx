b24eeea7712fdb8637c323bbbb61dbca06da8265
Dennis malware <davspencer30@gmail.com>
2023-09-27 18:23:58 +0300

    updates

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,6 +4,13 @@ const Hero = () => {
   return (
     <section id="home" className=" w-full bg-blue-900 flex flex-col justify-center gap-10 max-container">
       <div className="relative flex flex-col justify-center items-start w-full">
+        <h1 className="mt-10 font-poppins font-semibold text-7xl text-white ">
+        Don't settle for less.
+        </h1>
+        <p className="text-sm flex text-white leading-8 mt-6 mb-10 tracking-wider ">
+        We help personal injury victims with pre-settlement financial support so they can fight their legal battle and get what they are owed.
+        </p>
+    {/* here goes the input element section */}
 
       </div>
 
