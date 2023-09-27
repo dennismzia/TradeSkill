7f2cd3ff4540635d56fb5853397fc800eef4ac7c
Dennis malware <davspencer30@gmail.com>
2023-09-27 16:43:30 +0300

    updates

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({label}) => {
   return (
-    <button className="">
+    <button className="px-12 py-4 bg-purple-600 text-white max-w-3xl rounded-md hover:bg-green-500">
         {label}
     </button>
   )
