09bcd05ebb1e7052020a5bea89397ffdcb4b6716
Dennis malware <davspencer30@gmail.com>
2023-09-27 13:15:12 +0300

    updates

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import {NavLinks} from "../constants";
 
 const Nav = () => (
-    <header className='padding-x py-8 '>
+    <header className='padding-x py-8'>
+        <nav className='flex justify-between items-center max-container'>
+        <a href=""></a>
+
+        <ul className='flex-1 flex justify-center items-center'>
+            {
+                NavLinks.map((item) => (
+                    <li key={item.label}>
+                        <a href={item.href} className='font-poppins leading-normal text-sm'>
+                            {item.label}
+                        </a>
+
+                    </li>
+                ))
+            }
+
+        </ul>
+        </nav>
 
     </header>
 )
