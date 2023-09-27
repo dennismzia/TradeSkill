b76a9970e361035f1654ca89d25d2dd3799cea55
Dennis malware <davspencer30@gmail.com>
2023-09-27 18:14:36 +0300

    updates

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Hero = () => {
   return (
     <section id="home" className=" w-full bg-blue-900 flex flex-col justify-center gap-10 max-container">
-      
+      <div className="relative flex flex-col justify-center items-start w-full">
+
+      </div>
+
+      <div >
+
+      </div>
     </section>
   )
 }
