532572a68590aa061314c6a048c5e10e1bfbcc90
Dennis malware <davspencer30@gmail.com>
2023-09-27 13:29:44 +0300

    updats

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,9 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    './components/**/*.{html,js}',
   ],
   theme: {
-    extend: {
-      // colors: {
-      //   purple: "#943FF8",
-      // }
-    },
+    extend: {},
   },
   plugins: [],
 }
