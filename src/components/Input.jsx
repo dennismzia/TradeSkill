ee50f0f2dd2e6290eafddd1654cd6db0f61e5ec0
Dennis malware <davspencer30@gmail.com>
2023-09-28 14:02:33 +0300

    updates

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Input = ({label}) => {
   return (
       <div>
-        <p className="mt-8 mb-1">firstname</p>
+        <p className="mt-8 mb-1 text-sm text-[#33475b] font-semibold">first name</p>
       <input type="text" placeholder="firstnme"  className="mt-2 mb-2 border-b-4"/>
       </div>
 
