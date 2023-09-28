880914e4bf9774a509fd865417f4da16578152d5
Dennis malware <davspencer30@gmail.com>
2023-09-28 13:42:32 +0300

    updates

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,7 @@ const Input = ({label}) => {
   return (
       <div>
         <p className="mt-8 mb-1">firstname</p>
-      <input type="text" placeholder="firstnme"  className="mt-2 mb-2"/>
+      <input type="text" placeholder="firstnme"  className="mt-2 mb-2 border-b-4"/>
       </div>
 
   )
