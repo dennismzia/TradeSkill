ea73790b8d2b725f4c848d5dd17dee7fce33fc11
Dennis malware <davspencer30@gmail.com>
2023-09-29 10:57:55 +0300

    updates

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Input = ({label,inputType}) => {
   return (
-      <div className='padding-x '>
+      <div className='px-6 '>
         <p className="mt-8 mb-1 text-sm text-[#33475b] font-semibold">{label}</p>
       <input type={inputType} placeholder={inputType}  className="mt-2 mb-2 border-b-4"/>
       </div>
