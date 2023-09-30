87b25c00e70a95696286ac362d75b6b87cd35ca6
Dennis malware <davspencer30@gmail.com>
2023-09-30 09:58:53 +0300

    updates

--- src/sections/Fund.jsx
+++ src/sections/Fund.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Fund = () => {
   return (
-    <div>Fund</div>
+    <div className="padding-x padding-y">
+      <h2 className="font-semibold text-5xl text-black font-poppins mb-4">
+      Cases we fund
+      </h2>
+      <p className="text-[#757575] font-normal text-lg">
+      We partner with you for any of the following cases:
+
+      </p>
+    </div>
   )
 }
 
