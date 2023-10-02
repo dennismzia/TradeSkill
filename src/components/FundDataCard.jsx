266cfa4f4879911824b41bfacebf4924b970bfbb
Dennis malware <davspencer30@gmail.com>
2023-10-02 17:43:44 +0300

    updates

--- src/components/FundDataCard.jsx
+++ src/components/FundDataCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FundDataCard = ({imgurl,title}) => {
   return (
-    <div className="flex-col border border-[#DBDBE6] ms-4 hover:bg-gray-200" >
+    <div className="" >
         <img src={imgurl} alt="icon" width={80} height={80} className="mb-4" />
 
         <h2 className="font-semibold text-base leading-normal text-black font-poppins">
@@ -12,5 +12,7 @@ const FundDataCard = ({imgurl,title}) => {
     </div>
   )
 }
+
+// flex-col border border-[#DBDBE6] ms-4 hover:bg-gray-200
 
 export default FundDataCard
