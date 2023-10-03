b64cbd994ce34b1b8372db6b2c99b9a4927bf651
Dennis malware <davspencer30@gmail.com>
2023-10-03 11:49:49 +0300

    updates

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,10 +1,35 @@
+import { FooterLinks } from "../constants";
+import { Address } from "../constants";
 
 const Footer = () => {
   return (
-    <section id="footer" className=" w-full h-52 bg-[#1f2363]">
-      <div className="">
-        
+    <section id="footer" className=" w-full bg-[#1f2363] flex justify-between">
+      <div className="padding-x">
+        <h1 className="text-white font-extrabold py-4  font-poppins">
+        Corporate Office
+        </h1>
+
+        <p className="text-white ">
+        2 Wall St FL6, New York, NY 10005 
+        </p>
       </div>
+
+      <div className="padding mr-10">
+        <ul>
+          {
+            FooterLinks.map((link)=>(
+              <li key={link.label} className="mt-2 mb-4">
+                <a href={link.href} className="text-white tracking-wide font-poppins">
+                  {link.label}
+                </a>
+              </li>
+            ))
+
+          }
+        </ul>
+      </div>
+
+
 
     </section>
   )
