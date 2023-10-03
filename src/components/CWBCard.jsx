d2b68af18f3b80271dc206e9094de14b35e093ec
Dennis malware <davspencer30@gmail.com>
2023-10-03 10:42:53 +0300

    updates

---
+++ src/components/CWBCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CWBCard = ({}) => {
+  return (
+    <div>
+        <img src="" alt="" />
+        <h3></h3>
+        <p></p>
+    </div>
+  )
+}
+
+export default CWBCard
--- src/sections/Benefits.jsx
+++ src/sections/Benefits.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import CWBCard from "../components/CWBCard";
+import {Benefits} from "../constants";
 
 const Benefits = () => {
   return (
-    <div className="w-1/2 padding">
-      <h2 className="padding-x font-extrabold text-3xl text-black font-poppins ">
-        Cash with benefits
-      </h2>
-      <p className="padding text-lg text-slate-500">
-      More than just cash, we provide a support network to help you through one of the most challenging chapters of your life.
+    <section id='benefits' className="">
+      <div className="w-1/2 padding">
+        <h2 className="padding-x font-extrabold text-3xl text-black font-poppins ">
+          Cash with benefits
+        </h2>
+        <p className="padding text-lg text-slate-500">
+        More than just cash, we provide a support network to help you through one of the most challenging chapters of your life.
+        </p>
+      </div>
 
+      <div>
+        {
+          Benefits.map((item)=>(
+            <CWBCard imgurl={item.imgURL} label={item.label} subtext={item.subtext} />
+          ))
+        }
+      </div>
 
-      </p>
-    </div>
+    </section>
   )
 }
 
