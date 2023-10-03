6dbc49a46633ea8d131edf05bbf3e6d0fd0b9420
Dennis malware <davspencer30@gmail.com>
2023-10-03 10:14:48 +0300

    updates

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,13 +1,12 @@
 
 const CustomerReviews = () => {
   return (
-    <div className="w-1/2 flex-start flex-1 bg-slate-300">
-      <h2 className="font-poppins text-4xl text-black font-bold whitespace-normal">
+    <div className="w-1/2 padding flex-start flex-1 bg-gray-200">
+      <h2 className="padding-x font-poppins text-4xl text-black font-bold whitespace-normal">
       Hear straight from the 
       customers we finance.
-
       </h2>
-       
+  
     </div>
   )
 }
