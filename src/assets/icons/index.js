89710d2213890acc438901d30f4885ddfbe185ad
Dennis malware <davspencer30@gmail.com>
2023-10-03 10:51:41 +0300

    updates

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,13 +1,14 @@
 import icon1 from "./icon1.svg";
 import icon2 from "./icon2.svg";
 import icon3 from "./icon3.svg";
-import icon17 from "./icon17.svg";
 import icon16 from "./icon16.svg";
+import icon17 from "./icon17.svg";
 import icon18 from "./icon18.svg";
 export {
     icon1,
     icon2,   
     icon3,
+    icon16,
     icon17,
     icon18
 
