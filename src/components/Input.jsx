2a53fc8b1b9c6046aaa5843e15a1c690a1f08ee4
Dennis malware <davspencer30@gmail.com>
2023-10-04 15:05:23 +0300

    updates

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,7 @@ const Input = ({label,inputType}) => {
   return (
       <div className='px-6 '>
         <p className="mt-8 mb-1 text-sm text-[#33475b] font-semibold">{label}</p>
-      <input type={inputType} placeholder={inputType}  className="w-4/5 mt-2 mb-2 border-b-4  outline-none"/>
+      <input type={inputType} placeholder={inputType}  className="w-4/5 h-7 mt-2 mb-2 border-b-4 border-gray-200  outline-none"/>
       </div>
 
   )
