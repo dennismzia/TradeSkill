51b2788672912609e46c187d7cec5805cf785229
Dennis malware <davspencer30@gmail.com>
2023-10-06 19:21:44 +0300

    updates

--- src/components/HFCard.jsx
+++ src/components/HFCard.jsx
@@ -2,14 +2,14 @@ import { HardFactsData } from "../constants";
 
 const HFCard = () => {
   return (
-    <div>
+    <div className="mt-5">
       {
         HardFactsData.map((item)=>(
           <div>
-          <h2 className="text-7xl font-semibold text-[#1F2363]">
+          <h2 className="text-7xl font-semibold text-[#1F2363] mb-5">
             {item.label}
           </h2>
-          <p className="text-black text-sm font-poppins">
+          <p className="text-black text-sm font-poppins mt-2">
             {item.subtext}
           </p>
           </div>
--- src/sections/HardFacts.jsx
+++ src/sections/HardFacts.jsx
@@ -4,9 +4,9 @@ import {HFCard} from "../components";
 // TODO: make this card into a counter
 const HardFacts = () => {
   return (
-    <div className="flex padding-y">
+    <div className="flex">
       <img src={HardFactsimage} alt="image" className="px-20 mt-10" />
-      <div className="w-1/2 padding">
+      <div className="w-1/2 padding ">
         <h1 className="leading-normal text-black font-black text-4xl mb-9">
           THE HARD FACTS
         </h1>
