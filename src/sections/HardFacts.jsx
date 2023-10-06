d1e6ed57555544980f78b3434cca116369534dc6
Dennis malware <davspencer30@gmail.com>
2023-10-06 19:18:16 +0300

    updates

--- src/sections/HardFacts.jsx
+++ src/sections/HardFacts.jsx
@@ -4,7 +4,7 @@ import {HFCard} from "../components";
 // TODO: make this card into a counter
 const HardFacts = () => {
   return (
-    <div className="flex">
+    <div className="flex padding-y">
       <img src={HardFactsimage} alt="image" className="px-20 mt-10" />
       <div className="w-1/2 padding">
         <h1 className="leading-normal text-black font-black text-4xl mb-9">
