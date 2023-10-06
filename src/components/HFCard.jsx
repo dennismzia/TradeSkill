1b3b69a4c20659a9d2359e039f95185c68f6bea5
Dennis malware <davspencer30@gmail.com>
2023-10-06 16:11:36 +0300

    updates

--- src/components/HFCard.jsx
+++ src/components/HFCard.jsx
@@ -3,7 +3,18 @@ import { HardFactsData } from "../constants";
 const HFCard = () => {
   return (
     <div>
-
+      {
+        HardFactsData.map((item)=>(
+          <div>
+          <h2 className="text-7xl font-semibold text-[#1F2363]">
+            {item.label}
+          </h2>
+          <p className="text-black text-sm font-poppins">
+            {item.subtext}
+          </p>
+          </div>
+        ))
+      }
     </div>
   )
 }
--- src/sections/HardFacts.jsx
+++ src/sections/HardFacts.jsx
@@ -6,8 +6,8 @@ const HardFacts = () => {
   return (
     <div className="flex">
       <img src={HardFactsimage} alt="image" className="px-20 mt-10" />
-      <div>
-        <h1>
+      <div className="w-1/2 padding">
+        <h1 className="leading-normal text-black font-black text-4xl mb-9">
           THE HARD FACTS
         </h1>
         <HFCard/>
