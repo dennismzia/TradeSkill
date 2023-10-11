4707d3dc4e48180d9a36f56a9ff08cea40f14ca0
Dennis malware <davspencer30@gmail.com>
2023-10-11 19:16:29 +0300

    updates

--- src/components/HFCard.jsx
+++ src/components/HFCard.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
+import { ChevronLeft, ChevronRight } from "react-feather"
 import { HardFactsData } from "../constants";
 
 const HFCard = () => {
+  const [curr, setCurr] = useState(0)
+
+  const prev = () =>
+    setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1))
+  const next = () =>
+    setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1))
+
+    useEffect(() => {
+      if (!autoSlide) return
+      const slideInterval = setInterval(next, autoSlideInterval)
+      return () => clearInterval(slideInterval)
+    }, [])
+
   return (
     <div className="mt-5 w-full border flex flex-1">
       {
